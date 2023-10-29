abb1bbf02184e344e5c4e324481f935f8fe2f1b4
bobmcw <jasiekz2014@gmail.com>
2023-10-29 15:27:30 +0100

    added logic for creating a row of pixels

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector("div.container")
+
+let row = document.createElement("div")
+row.setAttribute("class","row")
+for (let i=0;i<16;i++)
+{
+    let square = document.createElement("div")
+    square.setAttribute("class","square")
+    row.appendChild(square)
+}
+container.appendChild(row)
